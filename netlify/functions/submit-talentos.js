1a85c9a884e10e5ef0abf89fb843835c0b8bbad9
ajovesbc-oss <ajoves.bc@gmail.com>
2026-04-29 21:43:25 -0400

    submit-talentos

---
+++ netlify/functions/submit-talentos.js
@@ -0,0 +1,70 @@
+const https = require('https');
+
+exports.handler = async (event) => {
+  if (event.httpMethod === 'OPTIONS') {
+    return {
+      statusCode: 200,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Headers': 'Content-Type',
+        'Access-Control-Allow-Methods': 'POST, OPTIONS'
+      },
+      body: ''
+    };
+  }
+
+  if (event.httpMethod !== 'POST') {
+    return { statusCode: 405, body: 'Method Not Allowed' };
+  }
+
+  const { fields } = JSON.parse(event.body);
+
+  const TOKEN = process.env.AIRTABLE_TOKEN;
+  const BASE_ID = process.env.AIRTABLE_BASE_ID;
+  const TABLE_ID = process.env.AIRTABLE_TABLE_ID_TALENTOS;
+
+  if (!TOKEN || !BASE_ID || !TABLE_ID) {
+    return {
+      statusCode: 500,
+      headers: { 'Access-Control-Allow-Origin': '*' },
+      body: JSON.stringify({ error: 'Variables de entorno no configuradas' })
+    };
+  }
+
+  const body = JSON.stringify({ fields });
+
+  return new Promise((resolve) => {
+    const options = {
+      hostname: 'api.airtable.com',
+      path: `/v0/${BASE_ID}/${TABLE_ID}`,
+      method: 'POST',
+      headers: {
+        'Authorization': `Bearer ${TOKEN}`,
+        'Content-Type': 'application/json',
+        'Content-Length': Buffer.byteLength(body)
+      }
+    };
+
+    const req = https.request(options, (res) => {
+      let data = '';
+      res.on('data', chunk => data += chunk);
+      res.on('end', () => resolve({
+        statusCode: res.statusCode,
+        headers: {
+          'Content-Type': 'application/json',
+          'Access-Control-Allow-Origin': '*'
+        },
+        body: data
+      }));
+    });
+
+    req.on('error', (err) => resolve({
+      statusCode: 500,
+      headers: { 'Access-Control-Allow-Origin': '*' },
+      body: JSON.stringify({ error: err.message })
+    }));
+
+    req.write(body);
+    req.end();
+  });
+};
